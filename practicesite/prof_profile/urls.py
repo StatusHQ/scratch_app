58103b71b24b4d89b76396465184463b6ebc279f
Charlie Gallentine <chgallentine@gmail.com>
2018-12-18 18:20:07 -0600

    Deletions work

--- practicesite/prof_profile/urls.py
+++ practicesite/prof_profile/urls.py
@@ -2,7 +2,7 @@
 # @Author: Charlie Gallentine
 # @Date:   2018-12-17 12:44:03
 # @Last Modified by:   Charlie Gallentine
-# @Last Modified time: 2018-12-18 16:02:09
+# @Last Modified time: 2018-12-18 16:32:22
 from django.urls import path
 from . import views
 from django.conf.urls import url
@@ -13,5 +13,6 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('<int:section_id>/', views.prof_section, name='prof_section'),
 	path('create/', views.prof_create, name='prof_section_create'),
-	path('<int:section_id>/edit/', views.prof_section_edit, name='prof_section_edit'),
+	path('<int:section_id>/add/', views.prof_section_add, name='prof_section_add'),
+	path('<int:section_id>/delete/', views.prof_section_del, name='prof_section_del'),
 ]
--- practicesite/prof_profile/views.py
+++ practicesite/prof_profile/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from .models import ProfileSection
+from .models import ProfileSection, ProfileEntry
 
 # Create your views here.
 
@@ -31,11 +31,13 @@ def prof_section(request, section_id):
 # 
 # 
 # Page to edit a section
-def prof_section_edit(request, section_id):
+def prof_section_add(request, section_id):
 	'''View function for home page of professional profile'''
 	section = get_object_or_404(ProfileSection, pk=section_id)
 	try:
-		section.profileentry_set.create(entry_name=request.POST['experience'])
+		section.profileentry_set.create(
+			entry_name=request.POST['experience'],
+			entry_text=request.POST['exp_description'])
 	except (ValueError, section.DoesNotExist):
 		return render(request, 'prof_profile/prof_section.html', {
 			'section' : section,
@@ -45,6 +47,21 @@ def prof_section_edit(request, section_id):
 		section.save()
 		return HttpResponseRedirect(reverse('prof_profile:prof_section', args=(section.id,)))
 
+def prof_section_del(request, section_id):
+	'''View function to remove an experience'''
+	section = get_object_or_404(ProfileSection, pk=section_id)
+	try:
+		selected_choice = section.profileentry_set.get(pk=request.POST['pk'])
+	except (KeyError, ProfileEntry.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'prof_profile/prof_section.html', {
+			'section': section,
+			'error_message': str(section_id) + ": doesn't exist.",
+		})
+	else:
+		selected_choice.delete()
+		section.save()
+		return HttpResponseRedirect(reverse('prof_profile:prof_section', args=(section.id,)))
 
 # 
 # 
@@ -52,3 +69,24 @@ def prof_section_edit(request, section_id):
 def prof_create(request):
 	'''View function for home page of professional profile'''
 	return HttpResponse("Hello, you creating a new section for your professional profile.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
